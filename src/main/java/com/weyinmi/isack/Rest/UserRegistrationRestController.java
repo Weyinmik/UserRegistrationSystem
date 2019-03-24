a8e39adcfbe5e29992fcfc7d7e22d5687fae15cf
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-24 02:08:42 +0100

    added @GetMapping to retrieve all users
    created a listAllUsers method in the controller class.

--- src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
+++ src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
@@ -1,17 +1,37 @@
 package com.weyinmi.isack.Rest;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.weyinmi.isack.UserRegistrationSystem.UserDTO;
 import com.weyinmi.isack.repository.UserJpaRepository;
+
 
 
 @RestController
 @RequestMapping("/api/user")
 public class UserRegistrationRestController {
-	public static final Logger logger = LoggerFactory.getLogger(UserRegistrationRestController.class);
+	public static final Logger logger = 
+			LoggerFactory.getLogger(UserRegistrationRestController.class);
 	private UserJpaRepository userJpaRepository;
+	
+	@Autowired
+	public void setUserJpaRepository(UserJpaRepository userJpaRepository) {
+		this.userJpaRepository = userJpaRepository;
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<List<UserDTO>> listAllUsers(){
+		List<UserDTO> users = userJpaRepository.findAll();
+		return new ResponseEntity<List<UserDTO>>(users, HttpStatus.OK);
+	}
 
 }
