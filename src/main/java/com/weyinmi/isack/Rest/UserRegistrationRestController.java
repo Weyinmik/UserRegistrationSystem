9d68c78f9914884c38c858ed87f6149f4b68c8cc
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-24 14:28:30 +0100

    Add a user to UserRegistrationSystem by implementing the POST verb functionality

--- src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
+++ src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
@@ -7,8 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,11 +33,18 @@ public class UserRegistrationRestController {
 		this.userJpaRepository = userJpaRepository;
 	}
 	
+	//	Fetch all users 
 	@GetMapping("/user")
 	public ResponseEntity<List<UsersDTO>> listAllUsers() {
 		List<UsersDTO> users = userJpaRepository.findAll();
 		return new ResponseEntity<List<UsersDTO>>(users, HttpStatus.OK);
 	}
-
+	
+	//	Add a user to UserRegistrationSystem by implementing the POST verb functionality.
+	@PostMapping(value="/user", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UsersDTO> createUser(@RequestBody final UsersDTO user){
+		return new ResponseEntity<UsersDTO>(user, HttpStatus.CREATED);
+		
+	}
 	
 }
