36dbe5f82758404e9815b575ab904fe1c69e73a2
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-24 01:55:33 +0100

    Update UserRegistrationRestController.java

--- src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
+++ src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
@@ -1,5 +1,17 @@
 package com.weyinmi.isack.Rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weyinmi.isack.repository.UserJpaRepository;
+
+
+@RestController
+@RequestMapping("/api/user")
 public class UserRegistrationRestController {
+	public static final Logger logger = LoggerFactory.getLogger(UserRegistrationRestController.class);
+	private UserJpaRepository userJpaRepository;
 
 }
