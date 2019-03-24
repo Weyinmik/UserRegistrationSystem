3f09817fc1b3c12af1b4f7603b3e36b7bf1f4545
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-24 18:56:31 +0100

    Update FieldValidationError.java

--- src/main/java/com/weyinmi/isack/Exception/FieldValidationError.java
+++ src/main/java/com/weyinmi/isack/Exception/FieldValidationError.java
@@ -1,5 +1,36 @@
 package com.weyinmi.isack.Exception;
 
+import java.awt.TrayIcon.MessageType;
+
 public class FieldValidationError {
+	
+	private String field;
+	private String message;
+	
+	private MessageType type;
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public MessageType getType() {
+		return type;
+	}
+
+	public void setType(MessageType type) {
+		this.type = type;
+	}
 
 }
