04db34c63a12250ce7333da55748fb03a3229488
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-24 15:20:22 +0100

    Implement an endpoint to update an individual user by using @PutMapping, added Postman Screenshot Result
    Also added screenshot Image result of the Postman Test

--- src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
+++ src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +55,25 @@ public class UserRegistrationRestController {
 	public ResponseEntity<UsersDTO> getUserById(@PathVariable("id") final Long id){
 		UsersDTO user = userJpaRepository.findById(id);
 		return new ResponseEntity<UsersDTO>(user, HttpStatus.OK);
+		
+	}
+	
+//	Implement an endpoint to update an individual user by using @PutMapping
+	@PutMapping(value = "/user/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UsersDTO> updateUser(@PathVariable("id") final Long id, @RequestBody UsersDTO user){
+		//	Fetch user based on id and set it to currentUser object of type UsersDTO
+		UsersDTO currentUser = userJpaRepository.findById(id);
+		
+		//	Update currentUser object data with user object data
+		currentUser.setName(user.getName());
+		currentUser.setAddress(user.getAddress());
+		currentUser.setEmail(user.getEmail());
+		
+		//	Save currentUser object
+		userJpaRepository.saveAndFlush(currentUser);
+		
+		//	Return Repository Object
+		return new ResponseEntity<UsersDTO>(currentUser, HttpStatus.OK);
 		
 	}
 	
