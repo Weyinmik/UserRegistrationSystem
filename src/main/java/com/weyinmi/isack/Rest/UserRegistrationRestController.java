2bb4e988d51432414a74bece70304523ea961ca9
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 04:26:52 +0100

    used preAuthorised Annotation and modified import.sql file

--- src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
+++ src/main/java/com/weyinmi/isack/Rest/UserRegistrationRestController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -108,6 +109,7 @@ public class UserRegistrationRestController {
 	
 //	Implement the delete functionality for an endpoint to delete an existing user from the UserRegistrationSystem by using @DeleteMapping
 	@DeleteMapping("/user/{id}")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<UsersDTO> deleteUser(@PathVariable("id") final Long id){
 		UsersDTO user = userJpaRepository.findById(id);
 		
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,3 @@
-INSERT INTO users (username, password, enabled, role) VALUES ('user', 'password', true, 'USER');
-
-INSERT INTO users (username, password, enabled, role) VALUES ('user', 'password', true, 'ADMIN');
-
-INSERT INTO users (username, password, enabled, role) VALUES ('user', 'password', true, 'USER');
+insert into users (username, password, enabled, role) values ('user', 'password', true, 'USER');
+insert into users (username, password, enabled, role) values ('admin', 'password', true, 'ADMIN');
+insert into users (username, password, enabled, role) values ('weyinmi', 'password', true, 'USER');
