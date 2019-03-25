e218ceba565e234c0a95e4b725b16f96126fdc94
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 02:59:00 +0100

    Generated getters and setters for instance variables in UserInfo class

--- src/main/java/com/weyinmi/isack/dto/UserInfo.java
+++ src/main/java/com/weyinmi/isack/dto/UserInfo.java
@@ -30,6 +30,48 @@ public class UserInfo {
 	
 	@Column(name="role")
 	private String role;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
 	
 
 }
