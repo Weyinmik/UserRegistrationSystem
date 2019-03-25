f7c6f5f0950d28c0fd4b1a0a2e27e42e28d090e1
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 02:36:39 +0100

    Update SpringSecurityConfiguration_InMemory.java
    Authentication customization with in-memory definition

--- src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_InMemory.java
+++ src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_InMemory.java
@@ -1,5 +1,26 @@
 package com.weyinmi.isack.Security;
 
-public class SpringSecurityConfiguration_InMemory {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SpringSecurityConfiguration_InMemory extends WebSecurityConfigurerAdapter{
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth)
+		throws Exception{ auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("password").roles("USER", "ADMIN");
+		
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.httpBasic().and().authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/app/user").hasRole("USER")
+		.antMatchers(HttpMethod.POST, "/app/user").hasRole("USER")
+		.antMatchers(HttpMethod.PUT, "/app/user/**").hasRole("USER")
+		.antMatchers(HttpMethod.DELETE, "/app/user**").hasRole("ADMIN")
+		.and().csrf().disable();
+	}
 
 }
