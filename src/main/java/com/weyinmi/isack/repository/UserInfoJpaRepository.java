d32ca644cd78f55b09a0aa806cf2992a54336fe5
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 03:09:11 +0100

    created import.sql test file
    to test the dtabase customization

--- src/main/java/com/weyinmi/isack/repository/UserInfoJpaRepository.java
+++ src/main/java/com/weyinmi/isack/repository/UserInfoJpaRepository.java
@@ -1,5 +1,11 @@
 package com.weyinmi.isack.repository;
 
-public interface UserInfoJpaRepository extends {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.weyinmi.isack.dto.UserInfo;
+
+@Repository
+public interface UserInfoJpaRepository extends JpaRepository<UserInfo, Long> {
 
 }
