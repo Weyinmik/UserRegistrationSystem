e1ef37c589d1dc68b7f6d1573f087af18ae932ac
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 05:46:05 +0100

    Update SpringSecurityConfiguration_InMemory.java

--- src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_InMemory.java
+++ src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_InMemory.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
@@ -19,13 +20,17 @@ public class SpringSecurityConfiguration_InMemory extends WebSecurityConfigurerA
 		
 	}
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http.httpBasic().and().authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/app/user").hasRole("USER")
-		.antMatchers(HttpMethod.POST, "/app/user").hasRole("USER")
-		.antMatchers(HttpMethod.PUT, "/app/user/**").hasRole("USER")
-		.antMatchers(HttpMethod.DELETE, "/app/user**").hasRole("ADMIN")
-		.and().csrf().disable();
-	}
+	protected void configure(HttpSecurity http) throws Exception {
 
+		http
+			.httpBasic()
+				.realmName("User Registration System")
+			.and()
+			.authorizeRequests()
+				.antMatchers("/login/login.html", "/template/home.html", "/").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
 }
