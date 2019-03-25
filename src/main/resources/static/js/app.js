6a8d9039c1d6fcc37be8394d0d3afd4b91938ce8
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 05:17:57 +0100

    Update app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,22 +1,43 @@
 var app = angular.module('userregistrationsystem', [ 'ngRoute', 'ngResource' ]);
 
-app.config(function($routeProvider) {
-	$routeProvider.when('/list-all-users', {
-		templateUrl : '/template/listuser.html',
-		controller : 'listUserController'
-	}).when('/register-new-user',{
-		templateUrl : '/template/userregistration.html',
-		controller : 'registerUserController'
-	}).when('/update-user/:id',{
-		templateUrl : '/template/userupdation.html' ,
-		controller : 'usersDetailsController'
-	}).otherwise({
-		redirectTo : '/home',
-		templateUrl : '/template/home.html',
-	});
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : '/template/home.html',
+			controller : 'homeController'
+		})
+		
+		.when('/list-all-users', {
+			templateUrl : '/template/listuser.html',
+			controller : 'listUserController'
+		})
+		
+		.when('/register-new-user',{
+			templateUrl : '/template/userregistration.html',
+			controller : 'registerUserController'
+		})
+		
+		.when('/update-user/:id',{
+			templateUrl : '/template/userupdation.html' ,
+			controller : 'usersDetailsController'
+		})
+		
+		.when('/login',{
+			templateUrl : '/login/login.html',
+			controller : 'loginController'
+		})
+		
+		.when('/logout',{
+			templateUrl : '/login/login.html',
+			controller : 'logoutController'
+		})
+		
+		.otherwise({
+			redirectTo : '/login'
+		});
 });
 
-// To register the AuthInterceptor with the AngularJS application
 app.config(['$httpProvider', function($httpProvider) {
-	  $httpProvider.interceptors.push('AuthInterceptor');
-	}]);
+  //$httpProvider.interceptors.push('AuthInterceptor');
+	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+}]);
