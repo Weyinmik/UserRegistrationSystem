1c415e8edcd690520fe5f424ce38b736f1c6d8ac
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 05:40:02 +0100

    Update ServiceAuthenticate.java

--- src/main/java/com/weyinmi/isack/Rest/ServiceAuthenticate.java
+++ src/main/java/com/weyinmi/isack/Rest/ServiceAuthenticate.java
@@ -1,5 +1,16 @@
 package com.weyinmi.isack.Rest;
 
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ServiceAuthenticate {
+	
+	@RequestMapping("/user")
+	  public Principal user(Principal user) {
+	    return user;
+	  }
 
 }
