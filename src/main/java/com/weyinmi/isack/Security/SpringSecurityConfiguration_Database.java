de7148e751f293a51307c3be0ced3abc68649137
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 04:01:42 +0100

    Update SpringSecurityConfiguration_Database.java

--- src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_Database.java
+++ src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_Database.java
@@ -1,5 +1,40 @@
 package com.weyinmi.isack.Security;
 
-public class SpringSecurityConfiguration_Database {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+import com.weyinmi.isack.Service.UserInfoDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfiguration_Database
+		extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserInfoDetailsService userInfoDetailsService;
+
+	@Override
+	protected void configure(
+			AuthenticationManagerBuilder authenticationManagerBuilder)
+			throws Exception {
+		authenticationManagerBuilder.userDetailsService(userInfoDetailsService);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests().antMatchers("/api/user/**").authenticated()
+				.and().authorizeRequests().antMatchers("/h2-console/**")
+				.authenticated().and().httpBasic()
+				.realmName("User Registration System").and().csrf().disable();
+	}
 }
