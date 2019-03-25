a1f120dd422fdf345d3287c6b0b8aac7da8c048e
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-25 04:31:50 +0100

    Update SpringSecurityConfiguration_InMemory.java

--- src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_InMemory.java
+++ src/main/java/com/weyinmi/isack/Security/SpringSecurityConfiguration_InMemory.java
@@ -1,11 +1,16 @@
 package com.weyinmi.isack.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SpringSecurityConfiguration_InMemory extends WebSecurityConfigurerAdapter{
 	@Autowired
 	protected void configureGlobal(AuthenticationManagerBuilder auth)
